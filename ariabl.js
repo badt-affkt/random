29ef12f1a3b42ab018c95982367912e8f2fd4650
BadtA <78870414+bnddd@users.noreply.github.com>
2024-03-14 09:42:21 +0700

    Create ariabl.js

---
+++ ariabl.js
@@ -0,0 +1,23 @@
+// Define two variables holding numbers
+var num1 = 10;
+var num2 = 5;
+
+// Perform arithmetic operations
+var sum = num1 + num2; // Addition
+var difference = num1 - num2; // Subtraction
+var product = num1 * num2; // Multiplication
+var quotient = num1 / num2; // Division
+
+// Output results to the console
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+
+// Increment and decrement
+num1++; // Increment by 1
+num2--; // Decrement by 1
+
+// Output incremented and decremented values
+console.log("Incremented num1: " + num1);
+console.log("Decremented num2: " + num2);
