135d97c34978488f2fdf4bc56bd92ffe4aeaa6a2
BadtA <78870414+bnddd@users.noreply.github.com>
2024-04-01 10:26:04 +0700

    Create Math.js

---
+++ Math.js
@@ -0,0 +1,11 @@
+public class MathFunctions {
+    public static void main(String[] args) {
+        double number = 16.0;
+        double squareRoot = Math.sqrt(number);
+        double absoluteValue = Math.abs(-10.5);
+        double power = Math.pow(2, 3);
+        System.out.println("Square root: " + squareRoot);
+        System.out.println("Absolute value: " + absoluteValue);
+        System.out.println("Power: " + power);
+    }
+}
