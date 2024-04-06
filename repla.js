751b0c2296b51d40588039b95eeaaf0bd2e2c35b
BadtA <78870414+bnddd@users.noreply.github.com>
2024-04-06 10:11:14 +0700

    Create repla.js

---
+++ repla.js
@@ -0,0 +1,7 @@
+public class StringReplaceAll {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String replacedStr = str.replaceAll("o", "X");
+        System.out.println("Replaced string: " + replacedStr);
+    }
+}
