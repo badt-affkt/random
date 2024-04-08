0447ed02ac04f08a65de322d66ba8b8fd44d27b6
BadtA <78870414+bnddd@users.noreply.github.com>
2024-04-08 10:09:19 +0700

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,7 @@
+public class LastIndexOfExample {
+    public static void main(String[] args) {
+        String str = "hello world, hello";
+        int lastIndex = str.lastIndexOf("hello");
+        System.out.println("Last index of 'hello': " + lastIndex);
+    }
+}
