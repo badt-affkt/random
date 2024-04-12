5d84b97d64129806adb0cac9382b6474d85c8c8d
BadtA <78870414+bnddd@users.noreply.github.com>
2024-04-12 12:44:09 +0700

    Create originalNumber.js

---
+++ originalNumber.js
@@ -0,0 +1,16 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int number = 12321;
+        int reversedNumber = 0, originalNumber = number;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+    }
+}
