bc161d11218059a2d71fb8696169746bdc66eb33
BadtA <78870414+bnddd@users.noreply.github.com>
2024-04-15 09:59:18 +0700

    Create Averagsdfsd.js

---
+++ Averagsdfsd.js
@@ -0,0 +1,7 @@
+public class AverageOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        double average = (num1 + num2) / 2.0;
+        System.out.println("Average of " + num1 + " and " + num2 + " is " + average);
+    }
+}
